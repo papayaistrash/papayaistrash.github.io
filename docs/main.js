28494931bd5afb3f056616e1139722d4289c722e
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2021-08-08 21:03:26 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,1 @@
-alert("hello world");
+<iframe src="https://open.spotify.com/embed/album/3e7IU11zNQe3GzMnGDeQOX" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
